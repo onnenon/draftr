4f87287b75c065b8aa4e4753dfdb16aaf503a428
Stephen <stephen.onnen@gmail.com>
2025-08-16 22:09:52 -0500

    update version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Draftr.MixProject do
     [
       app: :draftr,
       version: "0.1.0",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
